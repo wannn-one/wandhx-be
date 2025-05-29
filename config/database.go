bf90886a9adfeb99accc6b8a89dca197a5d50935
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2025-05-30 02:19:17 +0700

    fix: ipv4 supabase ping

--- config/database.go
+++ config/database.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -17,22 +16,19 @@ var DB *gorm.DB
 func InitDatabase() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: Error loading .env file, will use system environment variables")
 	}
 
-	supabaseUrl := os.Getenv("SUPABASE_URL")
-	if supabaseUrl == "" {
-		log.Fatal("SUPABASE_URL is required")
-	}
+	// Get database configuration from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
 
-	// Extract database URL from Supabase URL
-	// Supabase URL format: https://[project_id].supabase.co
-	projectID := strings.Split(strings.Split(supabaseUrl, "//")[1], ".")[0]
-	dbHost := fmt.Sprintf("db.%s.supabase.co", projectID)
-	dbUser := "postgres"
-	dbPassword := os.Getenv("SUPABASE_DB_PASSWORD") // This should be the database password, not the anon key
-	dbName := "postgres"
-	dbPort := "5432"
+	if dbHost == "" || dbUser == "" || dbPassword == "" || dbName == "" || dbPort == "" {
+		log.Fatal("Database configuration environment variables are required (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)")
+	}
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		dbHost, dbUser, dbPassword, dbName, dbPort)
