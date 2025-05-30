86a661e9b5bafa9fbea491035d60e415d49669cd
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2025-05-30 12:20:32 +0700

    fix: multiple connection prepared statement error

--- config/database.go
+++ config/database.go
@@ -30,13 +30,30 @@ func InitDatabase() {
 		log.Fatal("Database configuration environment variables are required (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require application_name=wandhx_be",
 		dbHost, dbUser, dbPassword, dbName, dbPort)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Configure GORM to handle prepared statements better
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+		PreferSimpleProtocol: true, // Disables implicit prepared statement usage
+	}), &gorm.Config{
+		PrepareStmt: false, // Disable prepared statement cache
+	})
+	
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
+
+	// Get the underlying SQL DB
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+
+	// Set connection pool settings
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
 
 	// Drop existing tables
 	err = db.Migrator().DropTable(&models.Experience{}, &models.Project{}, &models.SkillCategory{})
